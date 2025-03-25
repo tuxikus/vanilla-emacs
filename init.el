d649bc53049e921f3e0098c3037a5bcb14f20156
tuxikus <contact@tuxikus.de>
2025-03-25 21:31:32 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;; disable useless ui elements
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; dont create backup and lock files
+(setq create-lockfiles nil
+      make-backup-files nil)
+
+;; dont auto save buffers
+(auto-save-mode -1)
+
+;; dont show start screen
+(setq inhibit-startup-screen t
+      inhibit-startup-message t)
+
+;; save location in buffer
+(save-place-mode 1)
+
+;; content of initial scratch buffer
+(setq initial-scratch-message ";;; Emacs > Vi")
+
+;; use y and n instead of yes and no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; font
+(add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font-15"))
+
+;; enable pair completion
+(electric-pair-mode)
+
+;; ido - interactive do - configuration
+(setq ido-enable-flex-matching t
+      ido-everywhere t)
+(ido-mode t)
+
+;; icomplete configuration
+(icomplete-mode)
+
+;; keys
+(global-set-key (kbd "C-c d") 'duplicate-line)
+(global-set-key (kbd "C-c f") 'find-file)
+(global-set-key (kbd "C-c e r") 'eval-region)
+(global-set-key (kbd "C-c e b") 'eval-buffer)
